92d65898c1180085d1376d738fff2f4623c992a4
janhachoi <87477736+Junhachoi-DEV@users.noreply.github.com>
2022-12-21 15:23:36 +0900

    12-21 점프, 벽 레이케스트 추가

--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -10,7 +10,16 @@ public class player : MonoBehaviour
     public float run_speed = 6f;
     public float walk_speed= 3f;
 
-    float x; // 가로 이동
+    //점프
+    public float jump_force = 10f; //점프 힘 값
+
+    // 가로 이동 값
+    float x; 
+
+    //상태
+    bool is_trun; //앞 ,뒤 전환 상태
+    bool is_ground; //땅 상태
+    bool ray_wall; //벽 상태
 
     Rigidbody2D rigid; 
     void Start()
@@ -21,7 +30,8 @@ public class player : MonoBehaviour
 
     void Update()
     {
-        
+        check_wall_and_bottom();
+        player_jump();
     }
     // 이동은 효율을 위해 여기에 넣는다.
     void FixedUpdate()
@@ -39,6 +49,38 @@ public class player : MonoBehaviour
                 transform.localScale = new Vector3(x, 1, 1); //캐릭터 뒤집기
             }
             transform.Translate(new Vector3(x * apply_speed * Time.deltaTime, 0, 0));
+        }
+    }
+
+    //벽 && 바닥 체크.
+    void check_wall_and_bottom()
+    {
+        if (x != 0)
+        {
+            if (x == 1)
+            {
+                is_trun = true;
+            }
+            else
+            {
+                is_trun = false;
+            }
+        }
+        //땅체크
+        Debug.DrawRay(rigid.position, Vector2.down * 1f, new Color(0, 1, 0));
+        is_ground = Physics2D.Raycast(rigid.position, Vector2.down * 1f, 1f, LayerMask.GetMask("bottom"));
+        
+        //벽체크
+        float cont_num = 0.5f;
+        Debug.DrawRay(rigid.position, Vector2.right * (is_trun ? cont_num : cont_num * -1), new Color(0, 1, 0));
+        ray_wall = Physics2D.Raycast(rigid.position, Vector2.right * (is_trun ? cont_num : cont_num * -1), cont_num, LayerMask.GetMask("bottom"));
+    }
+
+    void player_jump()
+    {
+        if (Input.GetButtonDown("Jump") && is_ground)
+        {
+            rigid.velocity = new Vector2(rigid.velocity.x, jump_force);
         }
     }
 }

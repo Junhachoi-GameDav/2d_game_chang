6b26abee82b9b122234bfb85e4fc9c32bb53c21b
janhachoi <wnsgk7654@gmail.com>
2022-12-24 07:45:33 +0900

    12-24 갈고리 추가

---
+++ Assets/script/grapping.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grapping : MonoBehaviour
+{
+    public LineRenderer line;
+    public Transform hook;
+
+    public float hook_speed = 12f;
+    public float hook_distence = 3f;
+
+    bool is_hook_key_down;
+    bool is_line_max;
+    public bool is_attach;
+
+    Vector2 mouse_direction;
+
+    void Start()
+    {
+        //라인 그리기
+        line.positionCount = 2; //그려질 라인 포인트 개수
+        line.endWidth = line.startWidth = 0.05f; // 그려질 가로 길이
+        line.SetPosition(0, transform.position); //index 포인트, 위치
+        line.SetPosition(1, hook.position);
+        line.useWorldSpace = true; // 월드좌표로 한다는 뜻
+        is_attach = false;
+    }
+
+
+    void Update()
+    {
+        //위치를 계속 업데이트 해줘야 라인이 따라가져 보인다.
+        line.SetPosition(0, transform.position);
+        line.SetPosition(1, hook.position);
+
+        if (Input.GetKeyDown(KeyCode.E) && !is_hook_key_down) //e을 누르고 훅키를 안눌렀을때
+        {
+            hook.position = transform.position; // 누를시 처음위치
+            // 화면(스크린)월드 좌표에 마우스 위치에 케릭터 위치를 빼면 = 마우스의 방향
+            mouse_direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+            is_hook_key_down = true;   //키를 눌렀다 =true
+            is_line_max = false;       //아직 거리가 짧으니까
+            hook.gameObject.SetActive(true); // 활성화
+        }
+
+        if (is_hook_key_down && !is_line_max && !is_attach)// 눌렀고 false 이고 안붙었을때
+        {
+            //translate를 이용해서  (날아가는 방향.정규화 시키고 * 시간 * 스피드(힘))
+            hook.Translate(mouse_direction.normalized * Time.deltaTime * hook_speed);
+
+            if (Vector2.Distance(transform.position, hook.position) > hook_distence)// 후크의 거리가 hook_distence보다 클시
+            {
+                is_line_max = true;
+            }
+        }
+        else if (is_hook_key_down && is_line_max && !is_attach)// 눌렀고 true 안붙었을때
+        {
+            //movetowards는 타겟으로 가는 함수이다.
+            hook.position = Vector2.MoveTowards(hook.position, transform.position, Time.deltaTime * hook_speed);
+            if (Vector2.Distance(transform.position, hook.position) < 0.1f)//플레이어와의 거리가 0.1보다 작다면
+            {
+                is_hook_key_down = false;
+                is_line_max = false;
+                hook.gameObject.SetActive(false); // 활성화
+
+            }
+        }
+        else if (is_attach) //붙을때
+        {
+            if (Input.GetKeyDown(KeyCode.E)) // 붙은상태에서 다시 e를 누르면
+            {
+                is_attach = false;
+                is_hook_key_down = false;
+                is_line_max = false;
+                hook.GetComponent<hooking>().joint2D.enabled = false;
+                hook.gameObject.SetActive(false);
+            }
+
+        }
+    }
+}
---
+++ Assets/script/hooking.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hooking : MonoBehaviour
+{
+    grapping grap;
+    public DistanceJoint2D joint2D;
+    void Start()
+    {
+        //find 함수로 플레이어(이름)안에있는 컴포넌트에 접근한다.
+        grap = GameObject.Find("girl").GetComponent<grapping>();
+        //고리에 있는 조인트를 활성화
+        joint2D = GetComponent<DistanceJoint2D>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("ring")) //tag가 링일떄
+        {
+            joint2D.enabled = true; // 활성화
+            grap.is_attach = true;
+        }
+    }
+}

eb13a6f4f2be8e6f7704b34f56a8e0cdb2297651
janhachoi <wnsgk7654@gmail.com>
2022-12-29 00:39:00 +0900

    12-29 레이 거리 2로 변경

--- Assets/script/Enermy.cs
+++ Assets/script/Enermy.cs
@@ -53,7 +53,7 @@ public class Enermy : MonoBehaviour
         {
             
             RaycastHit2D raycast = Physics2D.Raycast(transform.position, transform.right * isLeft, distance, isLayer);
-            Debug.DrawRay(transform.position, Vector2.right * isLeft, new Color(0, 1, 0), 2);
+            Debug.DrawRay(transform.position, Vector2.right * isLeft * distance, new Color(0, 1, 0), 2);
             if (raycast.collider != null)
             {
                 Debug.Log("isfollow");

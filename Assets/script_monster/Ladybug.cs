0bf03147428a7945f2a2e5b9247e6ef8788e3c5e
ImGunHyoeng <102517991+ImGunHyoeng@users.noreply.github.com>
2023-01-26 23:08:37 +0900

    1-26(몬스터)

--- Assets/script_monster/Ladybug.cs
+++ Assets/script_monster/Ladybug.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Ladybug :Enermy
 {
     // Start is called before the first frame update
+    int weapon_damage;
+    GameObject effect;
     Animator animator;
     public Transform ray;
     int Hp = 5;
@@ -19,8 +21,11 @@ public class Ladybug :Enermy
         animator = GetComponent<Animator>();
         home = transform.position;//물체의 위치
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        GameObject weapon = Instantiate(prefab_weapon);
+        weapon_damage = weapon.GetComponent<granade>().granade_dmg;
+        effect = weapon.GetComponent<granade>().granade_effect;
     }
-
+ 
     // Update is called once per frame
     void Update()
     {
@@ -53,6 +58,7 @@ public class Ladybug :Enermy
                 StartCoroutine(Move());
             }
         }
+      //  Debug.Log(isLeft);
     }
 
     
@@ -87,14 +93,68 @@ public class Ladybug :Enermy
     {
         if (isEnd == false&&isFollow==true)
         {
+            DirectionEnemy(target.position.x, transform.position.x);
             if (Vector2.Distance(transform.position, target.position) > contactDistance && isFollow == true)
             {
+                
                 transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-                Debug.Log("follow");
+               // Debug.Log("follow");
+                //if () ;//플레이어가왼쪽에 있는지 오른쪽에 있는지를 알아야 한다.
             }
-            else if (Vector2.Distance(transform.position, target.position) < dontcatch && isFollow == true)
+            else if (Vector2.Distance(transform.position, target.position) < contactDistance && isFollow == true&&isDie==false)
             {
-                transform.Translate(Vector2.zero);
+                Attack();
+            }
+
+            if(isEnd == true)
+            {
+                speed += 1.5f;              
+            }
+           // Debug.Log(isLeft);
+            box.SetActive(false);
+        }
+    }
+
+
+    public void DirectionEnemy(float target, float basobj)
+    {
+        if (target < basobj)
+        {
+            animator.SetFloat("Isleft", -1);//방향의 설정
+            isLeft = -1;
+        }
+        else
+        {
+            animator.SetFloat("Isleft", 1);
+            isLeft = 1;
+        }
+    }
+
+    IEnumerator Die()
+    {
+        animator.SetTrigger("Die");
+        isDie = true;
+        //isDamage = true;//적 못움직이게
+        yield return new WaitForSeconds(3.5f);
+        //폭발하도록하기
+
+        Explosion();
+        yield return new WaitForSeconds(0.1f);
+        Destroy(gameObject);
+    }
+    public void Explosion()
+    {
+        expolsion.SetActive(true);
+        //if(Collision2D )
+        Collider2D[] collider2Ds = Physics2D.OverlapCircleAll(boxpos.position, 5f);
+        //박스의 위치와 박스의 크기에 그리고 회전값을 넣는다
+        foreach (Collider2D colider in collider2Ds)
+        {
+            // Debug.Log("충돌");
+            if (colider.tag == "Player")//콜라이더의 테그를 비교해서 플레이어면은 넣어놓는다
+            {
+                Debug.Log("explosion damage");
+                colider.GetComponent<Rigidbody2D>().AddForce(new Vector2(400f * isLeft, 500f));
             }
         }
     }
@@ -106,21 +166,56 @@ public class Ladybug :Enermy
         Debug.Log(Hp);
         if (Hp <= 0)
         {
-            Destroy(gameObject);
+            StartCoroutine(Die());
         }
        // return h;
     }
 
+    void attacked()
+    {
+        animator.SetBool("Attacked", false);
+    }
+
+    IEnumerator Attacked_weapon(GameObject collision)
+    {
+        iseffect = true;
+        Transform d = collision.transform;
+        Destroy(collision);
+        GameObject eff = Instantiate(effect, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(0.3f);
+        Destroy(eff);
+        yield return new WaitForSeconds(0.1f);
+        iseffect = false;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "weapon")
+        if (collision.tag == "weapon" && isDie == false)
         {
-
-            Debug.Log("damage");
-            TakeDamage(5, Hp);
+            if (collision.tag == "weapon")
+                StartCoroutine(Attacked_weapon(collision.gameObject));
+            Debug.Log("weapondamage");
+            TakeDamage(weapon_damage, Hp);
             isDamage = true;
             Debug.Log("isnot move");
+            animator.SetBool("Attacked", true);
+            Debug.Log(isDamage);
+            Invoke("attacked", 0.4f);
             Invoke("damage", 0.4f);
+
+        }
+        else if (collision.tag == "effect" && iseffect == false && isDie == false)
+        {
+            Debug.Log("effectdamage");
+            TakeDamage(weapon_damage, Hp);
+            isDamage = true;
+            Debug.Log("isnot move");
+            animator.SetBool("Attacked", true);
+            Debug.Log(isDamage);
+            Invoke("attacked", 0.4f);
+            Invoke("damage", 0.4f);
+            Debug.Log(isDamage);
+
         }
 
         if (collision.CompareTag("Player"))
@@ -129,6 +224,28 @@ public class Ladybug :Enermy
         }
     }
 
+    public Vector2 boxSize;
+    public Transform boxpos;
+    public GameObject expolsion;
+    public GameObject box;
+
+    public void Attack()
+    {
+        box.SetActive(true);
+        
+       
+        Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(boxpos.position, boxSize, 0);
+        //박스의 위치와 박스의 크기에 그리고 회전값을 넣는다
+        foreach (Collider2D colider in collider2Ds)
+        {
+            // Debug.Log("충돌");
+            if (colider.tag == "Player")//콜라이더의 테그를 비교해서 플레이어면은 넣어놓는다
+            {
+                Debug.Log("player damage");
+                colider.GetComponent<Rigidbody2D>().AddForce(new Vector2(200f * isLeft, 10f));
+            }
+        }
+    }
 
     private IEnumerator Move()
     {
@@ -147,4 +264,12 @@ public class Ladybug :Enermy
         yield return new WaitForSeconds(0.8f);
         isEnd = false;*/
     }
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(boxpos.position, new Vector2(1f, 1f));
+    }
+
 }
+
+

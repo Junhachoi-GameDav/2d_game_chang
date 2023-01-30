c6af93c285fe683db6548c87d864bfe004ca0217
janhachoi <87477736+Junhachoi-DEV@users.noreply.github.com>
2023-01-31 07:35:57 +0900

    1-31 데미지 매니져 생성

--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -314,6 +314,7 @@ public class player : MonoBehaviour
             anime.SetTrigger("is_hitted");
             gameObject.layer = 14; // 무적
             sprite.color = new Color(1, 1, 1, 0.5f); //투명해짐
+
             player_hp--; // 플레이어 hp 깍임. 나중에 매니저에서 관리 해도 됨.
             Invoke("hitted_deley", 0.3f);
             Invoke("hitted_back", 2f);

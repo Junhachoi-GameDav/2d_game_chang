edd77c271466e1b020130a9e69757ff007f753a4
janhachoi <87477736+Junhachoi-DEV@users.noreply.github.com>
2023-02-10 02:47:43 +0900

    2-10 페이드_인_아웃 메니져 추가

---
+++ Assets/script_manager/fade_manager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fade_manager : MonoBehaviour
+{
+    [Range(0.01f, 10f)] public float fade_time;
+    public Image fade_img;
+
+    //public bool bool_fade;
+    private void Awake()
+    {
+        fade_in();
+    }
+    /*
+    private void Update()
+    {
+        fade_in_out();
+    }
+
+    void fade_in_out()
+    {
+        Color color = fade_img.color;
+
+        if (color.a > 0 && bool_fade)
+        {
+            color.a -= Time.deltaTime;
+        }
+        else if(color.a < 1 && !bool_fade)
+        {
+            color.a += Time.deltaTime;
+        }
+        fade_img.color = color;
+    }
+    */
+
+    public void fade_in()
+    {
+        StartCoroutine(fade_in_out_co_ro(1, 0));
+    }
+    public void fade_out()
+    {
+        StartCoroutine(fade_in_out_co_ro(0, 1));
+    }
+    IEnumerator fade_in_out_co_ro(float start, float end)
+    {
+        float cur_time = 0f;
+        float percent = 0f;
+
+        while(percent < 1)
+        {
+            cur_time += Time.deltaTime;
+            percent = cur_time / fade_time;
+
+            Color color = fade_img.color;
+            color.a = Mathf.Lerp(start, end, percent);
+            fade_img.color = color;
+
+            yield return null;
+        }
+    }
+}

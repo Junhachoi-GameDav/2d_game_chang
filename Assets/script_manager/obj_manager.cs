c15d67b6a6396d58b1e352a72605a2e9ad53e73d
janhachoi <87477736+Junhachoi-DEV@users.noreply.github.com>
2023-02-19 13:22:05 +0900

    2-19

--- Assets/script_manager/obj_manager.cs
+++ Assets/script_manager/obj_manager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class obj_manager : MonoBehaviour
 {
+    public AudioClip main_menu_bgm;
+    public AudioClip story1_bgm;
+
+    AudioSource audio;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
